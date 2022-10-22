3a6bb0bbb956b1b137d085836c5b6c552d400f34
milan <pperisic.milan@gmail.com>
2022-10-22 18:15:05 +0200

    Interests Component

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Interests = () => {
   return (
-    <div>Interests</div>
+    <div className='interests'>
+      <h3>Interests</h3>
+      <p>Larsson made clear his intentions to take up coaching once his playing career came to an end, having also expressed an interest in returning to Celtic in a coaching capacity. Larsson stood out for his discipline, professionalism and work-rate throughout his career.</p>
+    </div>
   )
 }
 
