d35d854e04699f9be2e404202513668681535a84
milan <pperisic.milan@gmail.com>
2022-10-22 16:54:25 +0200

    Setting position, image and info text

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import Info from './components/Info';
 import About from './components/About';
 import Interests from './components/Interests';
 import Footer from './components/Footer';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Info />
-      <About/>
-      <Interests />
-      <Footer />
+    <div className="App container">
+      <div className="main">
+        <Info />
+        <About/>
+        <Interests />
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import profil from '../images/profil.png'
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <div className='info'>
+      <img src={profil} alt="Larsson" />
+      <h1>Henrik Larsson</h1>
+      <h2>Football player</h2>
+    </div>
   )
 }
 
