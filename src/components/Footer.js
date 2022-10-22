1be05c5450e15c833fe4b5f69543269eb8a7523b
milan <pperisic.milan@gmail.com>
2022-10-22 18:23:29 +0200

    Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <i className="fa-brands fa-square-twitter fa-2xl"></i>
+      <i className="fa-brands fa-square-facebook fa-2xl"></i>
+      <i className="fa-brands fa-square-instagram fa-2xl"></i>
+      <i className="fa-brands fa-linkedin fa-2xl"></i>
+    </div>
   )
 }
 
