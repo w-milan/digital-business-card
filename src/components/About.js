0714758f0f3fdf0acc17b1900f3110efea1fec72
milan <pperisic.milan@gmail.com>
2022-10-22 18:10:53 +0200

    About Component

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='about'>
+      <h3>About</h3>
+      <p>Edward Henrik Larsson (born 20 September 1971) is a Swedish professional football coach and former player, formerly an assistant manager of Barcelona.In his first season at the Celtic, he played a crucial role in Celtic winning their first league title in ten years. He suffered a broken leg in a UEFA Cup tie against Lyon in 1999. Larsson came back stronger, scoring 53 goals in a 2000–01 season that saw him win the European Golden Shoe. Larsson won four league titles in his seven years at Celtic.  His 242 goals in 313 matches saw Celtic fans nickname him The King of Kings. </p>
+    </div>
   )
 }
 
